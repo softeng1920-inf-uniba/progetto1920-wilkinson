e000744440fa0e2faf6c5fdc10af67d44c087b9b
pventrella20 <p.ventrella4@studenti.uniba.it>
2020-04-09 19:23:25 +0200

    ho iniziato a lavorare sulla classe Piece e Pawn

--- src/main/java/it/uniba/main/King.java
+++ src/main/java/it/uniba/main/King.java
@@ -2,10 +2,20 @@ package it.uniba.main;
 
 public class King extends Piece {
 
+	public King(boolean white) {
+		super(white);
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	public void draw() {
+		// TODO Auto-generated method stub
+		
+	}
+
 	@Override
 	boolean canMove(Board board, Spot start, Spot end) {
 		// TODO Auto-generated method stub
 		return false;
 	}
-
 }
