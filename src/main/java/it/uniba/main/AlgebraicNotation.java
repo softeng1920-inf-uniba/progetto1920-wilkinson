4914765fdf6d9e0b479ef1874b6ec83a124f1779
pventrella20 <p.ventrella4@studenti.uniba.it>
2020-04-09 17:57:04 +0200

    creata la classe perla gestione di un comando in notazione algebrica, con la sua relativa classe di test

--- src/main/java/it/uniba/main/AlgebraicNotation.java
+++ src/main/java/it/uniba/main/AlgebraicNotation.java
@@ -5,25 +5,25 @@ import java.util.ArrayList;
 
 public class AlgebraicNotation {
 	String pieceLetter = "";	//lettera corrispondente al pezzo 
-	ArrayList<String> symbol = new ArrayList<String>();	//lista di simboli speciali nel comando
+	ArrayList<String> symbol = new ArrayList<String>();	//simbolo speciale nel comando
 	String endSquareId;	//casa di arrivo del pezzo
 	ArrayList<String> symbolList = new ArrayList<String>();	//simboli possibili
-	boolean isCastle = false;	//controllo se arrocco
-	boolean isEnPassant = false;	//controllo se en passant
-	boolean isDoubleCheck = false;	//
+	boolean isCastle = false;
+	boolean isEnPassant = false;
+	boolean isDoubleCheck = false;
 	
-	/**costruttore che fa partire i metodi che dividono e interpretano il comando in notazione algebrica
-	 * 
-	 * @param command comando in notazione algebrica derivato dallo standard di input (utente)
-	 */
+	private static final int STARTINDEX = 0;
+	private static final int PIECELETTERINDEX = 1;
+	private static final int DOUBLECHECKSPOTS = 2;
+	private static final int DOUBLECHECKINDEX = 3;
+	private static final int ENPASSANTINDEX = 4;
+	private static final int STARTINDEXCASTLING = 5;
+
 	AlgebraicNotation(String command){
 		initializeSymbolList();
 		divideCommand(command);
 	}
 
-	/**inizializza la lista dei simboli speciali ritrovabili nel comando
-	 * 
-	 */
 	void initializeSymbolList() {	//elenco simboli possibili
 		symbolList.add("+");
 		symbolList.add("x");
@@ -34,52 +34,120 @@ public class AlgebraicNotation {
 		symbolList.add("0-0");
 	}
 
-	/**metodo che avvalora (a partire dal comando) gli attributi di classe 
-	 * 
-	 * @param command comando in notazione algebrica
-	 */
 	void divideCommand(String command){
-		//TODO
-
-	}
-
-	/**controlla se il pezzo da muovere è un pedone 
-	 * 
-	 * @param command comando in notazione algebrica
-	 * @return true se mossa di un pedone, false altrimenti
-	 */
-	boolean isPawn(String command) {	//controllo se la mossa è di un pedone (nessuna lettera iniziale)
-		//TODO
-		return false;
-	}
-
-	/**metodo che estrare gli eventuali simboli speciali dal comando
-	 * 
-	 * @param command comando in notazione algebrica
-	 */
-	void extractSymbol(String command) {	//estrae l'eventuale simbolo speciale 
-		//TODO
-	}
-
-	/**metodo che toglie dalla stringa comando un altro simbolo o una lista di simboli speciali
-	 * 
-	 * @param command comando in notazione algebrica
-	 * @param extracted simbolo(i) da rimuovere
-	 * @return newCommand stringa "differenza"
-	 */
-	String reduceString(String command, String extracted) {	//riduce la stringa di comando eliminando i caratteri già estratti
-		String newCommand = "";
-		//TODO
-		return newCommand;
+		if(!isPawn(command)) {	//controllo lettera se non pedone
+			this.pieceLetter = command.substring(STARTINDEX,PIECELETTERINDEX);
+			command = reduceString(command, getPieceLetter());
+			}
 		
+		extractSymbol(command);	//estrazione simbolo speciale
+		
+		if(isDoubleCheck) {	//caso scacco doppio
+			command = command.substring(STARTINDEX, command.length()-DOUBLECHECKSPOTS);
+		}
+		
+		for(String currentSymbol: getSymbol()) {	//elimino eventuali altri simboli
+			command = reduceString(command, currentSymbol);
+		}
+		
+		if(isEnPassant) {	//caso en passant
+			setEndSquareId(command.substring(STARTINDEX, command.length()-ENPASSANTINDEX)); //elimino la dicitura e.p. 
+		} else if(isCastle) {	//caso arrocco
+			setEndSquareId("");
+		} else {
+			setEndSquareId(command);	//il resto della stringa è la casella di partenza/arrivo
+		}
+
 	}
 
-	/**metodo che stampa a video il comando scomposto (per testarlo) 
-	 * 
-	 */
+	boolean isPawn(String command) {	//controllo se la mossa è di un pedone (nessuna lettera iniziale)
+
+		char firstLetter = command.charAt(STARTINDEX);
+		if(Character.isUpperCase(firstLetter)) {
+			return false;
+		}
+
+		return true;
+	}
+
+
+	void extractSymbol(String command) {	//estrae l'eventuale simbolo speciale 
+		int pos = 0;
+
+		for(String currentSymbol: symbolList) {
+			if(command.contains(currentSymbol)) {
+	
+				if(pos >= STARTINDEXCASTLING) {	//check di simboli che innescano eventi
+					this.isCastle = true;
+				} else if(pos == ENPASSANTINDEX) {
+					this.isEnPassant = true;
+				} else if(pos == DOUBLECHECKINDEX) {
+					this.isDoubleCheck = true;
+					getSymbol().remove(symbolList.get(STARTINDEX));
+				}
+				
+				getSymbol().add(currentSymbol);
+				
+				if(isCastle) {	//interrompo se arrocco
+					break;
+				}
+			}
+			pos++;
+		}
+	}
+
+	String reduceString(String command, String extracted) {	//riduce la stringa di comando eliminando i caratteri già estratti
+
+		/*controllo se la stringa da sottrarre è vuota o se contiene un simbolo da trattare diversamente*/
+		if(!extracted.isEmpty() && !(extracted == symbolList.get(ENPASSANTINDEX)) && !(extracted == symbolList.get(DOUBLECHECKINDEX))) {
+			String newCommand = "";
+
+			for(int i=0; i<command.length(); i++) {
+				char commandToken = command.charAt(i);
+				
+				for(int j=0; j<extracted.length(); j++) {
+					char extractedToken = extracted.charAt(j);
+					
+					if(commandToken != extractedToken) {
+						newCommand += commandToken;
+					}
+				}
+			}
+			return newCommand;
+		}
+		return command;
+	}
+
+
 	public String toString(){	//stampa a video del comando scomposto
 		String output = "";
-		//TODO
+		output += "Nome Pezzo: "+getPieceLetter()+"\nCasa di arrivo: "+getEndSquareId()+"\nSimbolo speciale: "+getSymbol();
 		return output;
+	}
+
+	
+	/*Getters & Setters*/
+	public String getPieceLetter() {
+		return pieceLetter;
+	}
+
+	public void setPieceLetter(String pieceLetter) {
+		this.pieceLetter = pieceLetter;
+	}
+
+	public ArrayList<String> getSymbol() {
+		return symbol;
+	}
+
+	public void setSymbol(ArrayList<String> symbol) {
+		this.symbol = symbol;
+	}
+
+	public String getEndSquareId() {
+		return endSquareId;
+	}
+
+	public void setEndSquareId(String endSquareId) {
+		this.endSquareId = endSquareId;
 	}
 }
