67bc700bde681427443256d26fbde63cb5557460
Giuseppe Sancesario <giuseppesancesario1999@outlook.com>
2020-04-10 17:12:12 +0200

    implementati metodi draw e canMove di Pawn sono state implementati i metodi draw per la visualizzazione dell'unicode dei pezzi pedone bianco o nero, ed il metodo canMove per stabilire se e' consentito il movimento in avanti del pedone. L'implementazione della cattura e quindi del movimento in obliquo deve essere ancora implementato.

--- src/main/java/it/uniba/main/Pawn.java
+++ src/main/java/it/uniba/main/Pawn.java
@@ -9,33 +9,49 @@ public class Pawn extends Piece {
 
 	@Override
 	public void draw() {
-		// TODO Auto-generated method stub
-		
+		if(isWhite())
+		{
+			System.out.println("Il pedone e' di colore bianco ed il suo codice Unicode e': u2659");
+		}
+		else
+		{
+			System.out.println("Il pedone e' di colore nero ed il suo codice Unicode e': u265f");
+		}
+
 	}
-	
+
 	@Override
 	boolean canMove(Board board, Spot start, Spot end) {
-		
+
 		//non posso muovere in una casella dove c'Ã¨ un pezzo dello stesso colore
 		if(end.piece.isWhite() == this.isWhite()) {
 			return false;
 		}
-		
 		if(start.piece.isWhite()) {
-			if((start.y == end.y) || (start.x == (end.x +1))) {
-				//TODO movimento in avanti del pedone bianco
-				return true;
+			if((start.y == end.y) && (start.x == (end.x +1))) {
+				if(end.piece==null) //verifico che nello spot d'arrivo non siano già presenti pezzi
+				{
+					return true;
+				}
+				else 
+				{
+					return false;
+				}
+				//TODO movimento in avanti del pedone bianco e possibile cattura in obliquo
 			}
 		} else if(!start.piece.isWhite()) {
-			if((start.y == end.y) || (start.x == (end.x -1))) {
-				//TODO movimento in avanti del pedone nero
-				return true;
+			if((start.y == end.y) && (start.x == (end.x -1))) {
+				if(end.piece==null) //verifico che nello spot d'arrivo non siano già presenti pezzi
+				{
+					return true;
+				}
+				else 
+				{
+					return false;
+				}
 			}
+			//TODO movimento in avanti del pedone nero e possibile cattura in obliquo
 		}      
-		
 		return false;
 	}
-
-	
-
 }
