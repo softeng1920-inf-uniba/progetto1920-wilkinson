77925c5d92db3dd0e6c5c8eb74a9774ac86643c3
Perru21 <andreaperruggini@gmail.com>
2020-04-11 19:35:15 +0200

    Creata la scacchiera e aggiunti i relativi pezzi in essa

--- src/main/java/it/uniba/main/Board.java
+++ src/main/java/it/uniba/main/Board.java
@@ -27,73 +27,45 @@ public class Board {
 	 * 
 	 */
 	void resetBoard() {
-		boxes[1][0] = new Spot(1,0, new Pawn());
-		boxes[1][1] = new Spot(1,1, new Pawn());
-		boxes[1][2] = new Spot(1,2, new Pawn());
-		boxes[1][3] = new Spot(1,3, new Pawn());
-		boxes[1][4] = new Spot(1,4, new Pawn());
-		boxes[1][5] = new Spot(1,5, new Pawn());
-		boxes[1][6] = new Spot(1,6, new Pawn());
-		boxes[1][7] = new Spot(1,7, new Pawn());
-		boxes[6][0] = new Spot(6,0, new Pawn());
-		boxes[6][1] = new Spot(6,1, new Pawn());
-		boxes[6][2] = new Spot(6,2, new Pawn());
-		boxes[6][3] = new Spot(6,4, new Pawn());
-		boxes[6][4] = new Spot(6,4, new Pawn());
-		boxes[6][5] = new Spot(6,5, new Pawn());
-		boxes[6][6] = new Spot(6,6, new Pawn());
-		boxes[6][7] = new Spot(6,7, new Pawn());
-		boxes[0][0] = new Spot(0,0, new Rook());
-		boxes[0][7] = new Spot(0,7, new Rook());
-		boxes[7][0] = new Spot(7,0, new Rook());
-		boxes[7][7] = new Spot(7,7, new Rook());
-		boxes[0][1] = new Spot(0,1, new Knight());
-		boxes[0][6] = new Spot(0,6, new Knight());
-		boxes[7][1] = new Spot(7,1, new Knight());
-		boxes[7][6] = new Spot(7,6, new Knight());
-		boxes[0][2] = new Spot(0,2, new Bishop());
-		boxes[0][5] = new Spot(0,5, new Bishop());
-		boxes[7][2] = new Spot(7,2, new Bishop());
-		boxes[7][5] = new Spot(7,5, new Bishop());
-		boxes[0][3] = new Spot(0,3, new Queen());
-		boxes[7][3] = new Spot(7,3, new Queen());
-		boxes[0][4] = new Spot(0,4, new King());
-		boxes[7][4] = new Spot(7,4, new King());
-		boxes[2][0] = new Spot(2,0, null);
-		boxes[2][1] = new Spot(2,1, null);
-		boxes[2][2] = new Spot(2,2, null);
-		boxes[2][3] = new Spot(2,3, null);
-		boxes[2][4] = new Spot(2,4, null);
-		boxes[2][5] = new Spot(2,5, null);
-		boxes[2][6] = new Spot(2,6, null);
-		boxes[2][7] = new Spot(2,7, null);
-		boxes[3][0] = new Spot(3,0, null);
-		boxes[3][1] = new Spot(3,1, null);
-		boxes[3][2] = new Spot(3,2, null);
-		boxes[3][3] = new Spot(3,3, null);
-		boxes[3][4] = new Spot(3,4, null);
-		boxes[3][5] = new Spot(3,5, null);
-		boxes[3][6] = new Spot(3,6, null);
-		boxes[3][7] = new Spot(3,7, null);
-		boxes[4][0] = new Spot(4,0, null);
-		boxes[4][1] = new Spot(4,1, null);
-		boxes[4][2] = new Spot(4,2, null);
-		boxes[4][3] = new Spot(4,3, null);
-		boxes[4][4] = new Spot(4,4, null);
-		boxes[4][5] = new Spot(4,5, null);
-		boxes[4][6] = new Spot(4,6, null);
-		boxes[4][7] = new Spot(4,7, null);
-		boxes[5][0] = new Spot(5,0, null);
-		boxes[5][1] = new Spot(5,1, null);
-		boxes[5][2] = new Spot(5,2, null);
-		boxes[5][3] = new Spot(5,3, null);
-		boxes[5][4] = new Spot(5,4, null);
-		boxes[5][5] = new Spot(5,5, null);
-		boxes[5][6] = new Spot(5,6, null);
-		boxes[5][7] = new Spot(5,7, null);
-		System.out.println(boxes);
-
+		boxes[1][0] = new Spot(1,0, new Pawn(false));
+		boxes[1][1] = new Spot(1,1, new Pawn(false));
+		boxes[1][2] = new Spot(1,2, new Pawn(false));
+		boxes[1][3] = new Spot(1,3, new Pawn(false));
+		boxes[1][4] = new Spot(1,4, new Pawn(false));
+		boxes[1][5] = new Spot(1,5, new Pawn(false));
+		boxes[1][6] = new Spot(1,6, new Pawn(false));
+		boxes[1][7] = new Spot(1,7, new Pawn(false));
+		boxes[6][0] = new Spot(6,0, new Pawn(true));
+		boxes[6][1] = new Spot(6,1, new Pawn(true));
+		boxes[6][2] = new Spot(6,2, new Pawn(true));
+		boxes[6][3] = new Spot(6,4, new Pawn(true));
+		boxes[6][4] = new Spot(6,4, new Pawn(true));
+		boxes[6][5] = new Spot(6,5, new Pawn(true));
+		boxes[6][6] = new Spot(6,6, new Pawn(true));
+		boxes[6][7] = new Spot(6,7, new Pawn(true));
+		boxes[0][0] = new Spot(0,0, new Rook(false));
+		boxes[0][7] = new Spot(0,7, new Rook(false));
+		boxes[7][0] = new Spot(7,0, new Rook(true));
+		boxes[7][7] = new Spot(7,7, new Rook(true));
+		boxes[0][1] = new Spot(0,1, new Knight(false));
+		boxes[0][6] = new Spot(0,6, new Knight(false));
+		boxes[7][1] = new Spot(7,1, new Knight(true));
+		boxes[7][6] = new Spot(7,6, new Knight(true));
+		boxes[0][2] = new Spot(0,2, new Bishop(false));
+		boxes[0][5] = new Spot(0,5, new Bishop(false));
+		boxes[7][2] = new Spot(7,2, new Bishop(true));
+		boxes[7][5] = new Spot(7,5, new Bishop(true));
+		boxes[0][3] = new Spot(0,3, new Queen(false));
+		boxes[7][3] = new Spot(7,3, new Queen(true));
+		boxes[0][4] = new Spot(0,4, new King(false));
+		boxes[7][4] = new Spot(7,4, new King(true));
+		for (int x=2; x<6; x++) {
+			for (int j=0; j<8; j++) {
+				boxes[x][j] = new Spot(x, j, null);
+			}
+		}
 	}
+	
 
 	/**metodo che permette la stampa a video della scacchiera nella configurazione attuale
 	 * 
