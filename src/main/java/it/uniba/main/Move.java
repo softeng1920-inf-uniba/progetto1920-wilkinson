661b1a8ec60920f40a75b5c6d2fcf50e38253529
Gianluca Laera <g.laera28@studenti.uniba.it>
2020-04-11 11:11:09 +0200

    Implementata classe Move.java, che gestirà i movimenti dei pezzi, ed apportate correzioni ad essa

--- src/main/java/it/uniba/main/Move.java
+++ src/main/java/it/uniba/main/Move.java
@@ -5,64 +5,60 @@ public class Move {
   private final Spot start; // casa di partenza
   private final Spot end; // casa di arrivo
   private final Piece pieceMoved; // pezzo che deve eseguire il movimento
-	
-	/*di regola pieceKilled e coordinateKilled sono inutili, perchè in questa classe bisogna solo riportare nei membri ciò che interpreta 
-	* interpreter, poi la mossa verrà effettuata con un altro metodo della classe Game (il ragionamento è giusto, però verrà fatto successivamente insomma)*/
-  private Piece pieceKilled = null; // eventuale pezzo catturato col movimento
-  private Spot coordinateKilling = null; // coordinate in cui avviene la cattura, non sono sicuro che questo serva...
-
-	/*per ciò che ho scritto sopra il costruttore non tiene conto della cattura o meno, ne basta solo uno che prende command al suo interno)*/
-  // Overide del costruttore. Movimento senza cattura
-  public Move(String command) {
-    this(command, null);
-  }
-
+  
   /**
    * costruttore dell'oggetto Move
    * 
    * @param command comando da interpretare in mossa
    */
   // Movimento con cattura
-  public Move(final String command, Spot spotKilling) {				//Probabilmente andrebbe messo private
-    this.interpreter = new AlgebraicNotation(command); 				// Istanzio l'oggetto interpreter
-    this.start = // TODO							//prende la casella di partenza
-    this.pieceMoved = interpreter.getPieceLetter();					//prende il pezzo che si muove
-	  				/*prima va interpretata la lettera e capito che pezzo è*/
-	this.end = interpreter.getEndSquareId();						//prende la casella d'arrivo
-	  				/*con interpreter.getEndSquareId() viene tirata fuori una stringa
-					* ad esempio "e4", bisogna convertirla in Spot, quindi capire indice di riga e colonna
-					* (la riga è 4, la colonna è e [quindi 5]) e inizializzare this.end con il costruttore di Spot
-					* cioè this.end = new Spot(indiceriga, indicecolonna, null)
-					*/
-	
-	//verifico se sta avvenendo una cattura, se true, prendo il pezzo catturato e la posizione della cattura
-	if (spotKilling != null) {
-	  this.pieceKilled = spotKilling.getPieceKilled();											
-      this.coordinateKilling = spotKilling.getCoordinate();									
-    }
+  public Move(final String command) {				                                                                        //Probabilmente andrebbe messo private
+    this.interpreter = new AlgebraicNotation(command); 				                                                     // Istanzio l'oggetto interpreter
+    String algebraicPieceMoved = interpreter.getPieceLetter();                                                    // Stringa in notazione algebrica del pezzo che deve muoversi
+    String algebraicFinalSpot = interpreter.getEndSquareId();                                                    // Stringa della posizione d'arrivo in notazione algebrica che deve essere convertita
+    this.start = new Spot ();// TODO											                                                      // prende la casella di partenza
+    this.pieceMoved = new Piece (setPieceMoved());				                                       	             // prende il pezzo che si muove
+    this.end = new Spot (coordinateRow(algebraicFinalSpot) , coordinateColumn(algebraicFinalSpot), null);     // prende la casella d'arrivo
   }
-
-  // Controlla se si il movimento è una cattura, servirà per le sottoclassi
-	/*questo metodo semplicemente ritorna il valore di isKilled di pieceMoved, che sarà vero o falso se il pezzo è settato 
-	* catturato o meno*/
-  public boolean isKilled() {
-    if (pieceKilled == null) {
-      return false;
-	} 
-	else {
-      return true;
-    }
-  }
-  //Pezzo catturato
-  public Piece getPieceKilled() {
-    return pieceKilled;
-  }
-  //Coordinate della cattura
-  public Spot getCoordinateKilling() {		//Ho messo Spot, ma in realtà non sono sicuro che debba prenderli da qui
-    return coordinateKilling;
-  }
-
   
+	// Metodo che trasforma la colonna passata come lettera nel corrispondente indice numerico che identifica la colonna della matrice scacchiera
+  public int coordinateColumn(String columnFinalSpot){
+    char charColumn = algebraicFinalSpot.charAt(0);
+    int coordinateY = charColumn - 'a' + 1;
+    return coordinateY;
+  }
+
+  // Metodo che trasforma la riga passata come stringa nell'intero, che identifica la riga della matrice della matrice scacchiera
+  public int coordinateRow(String rowFinalSpot){
+    int coordinateX = Integer.parseInt(algebraicFinalSpot.substring(1,2));
+    return coordinateX;
+  }
+
+  //Metodo che acquisisce la dicitura in notazione algebrica ed interpreta il pezzo che deve muoversi
+  public void setPieceMoved (){
+    switch (algebraicPieceMoved){          
+      case null:
+        pieceMoved = new Pawn();
+        break;
+      case 'T':
+        pieceMoved = new Rook();
+        break;
+      case 'C':
+        pieceMoved = new Knight();
+        break;
+      case 'A':
+        pieceMoved = new Bishop();
+        break;
+      case 'D':
+        pieceMoved = new Queen();
+        break;
+      case 'R':
+        pieceMoved = new King();
+        break;
+      default :
+
+    }
+  }
 
   /**
    * enumerazione dello stato di gioco (per verificare se la partita è ancora in
