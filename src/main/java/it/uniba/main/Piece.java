eb687885a71a3f7ef7417bd653726e91a45fe66b
Giuseppe Sancesario <giuseppesancesario1999@outlook.com>
2020-04-15 13:01:15 +0200

    Implementata classe Pawn.java che controlla movimento legale del pedone

--- src/main/java/it/uniba/main/Piece.java
+++ src/main/java/it/uniba/main/Piece.java
@@ -2,7 +2,7 @@ package it.uniba.main;
 
 public abstract class Piece {
 	boolean killed = false;	//true se pezzo catturato, false altrimenti
-	boolean white;	//true se pezzo bianco, false altrimenti
+	boolean white = false;	//true se pezzo bianco, false altrimenti
 	boolean moved = false;  //true se pezzo mosso, false altrimenti
 	
 	/**costruttore della classe Pezzo
@@ -15,7 +15,7 @@ public abstract class Piece {
   
 	/**ritorna il valore dell'attributo ehite
 	 * 
-	 * @return white true se il pezzo Ã¨ bianco, false se il pezzo Ã¨ nero
+	 * @return white true se il pezzo è bianco, false se il pezzo è nero
 	 */
     public boolean isWhite() { 
         return this.white; 
@@ -31,7 +31,7 @@ public abstract class Piece {
   
     /**ritorna il valore dell'attributo killed
      * 
-     * @return killed true se Ã¨ stato catturato, false altrimenti
+     * @return killed true se è stato catturato, false altrimenti
      */
     public boolean isKilled() { 
         return this.killed; 
@@ -46,7 +46,7 @@ public abstract class Piece {
     
     /**ritorna il valore dell'attributo moved
      * 
-     * @return moved true se il pezzo Ã¨ stato mosso, false altrimenti
+     * @return moved true se il pezzo è stato mosso, false altrimenti
      */
     public boolean isMoved() {
     	return this.moved;
@@ -60,12 +60,12 @@ public abstract class Piece {
     }
     
     /**metodo che stampa a console il corrispondente carattere Unicode del pezzo
-     * stabilendo se Ã¨ nero o bianco
-     * @return 
+     * stabilendo se è nero o bianco
+<<<<<<< HEAD
      */
     public abstract String draw();
 	
-	/**metodo che stabilisce se il pezzo puÃ² muoversi (la mossa Ã¨ legale)
+	/**metodo che stabilisce se il pezzo può muoversi (la mossa è legale)
 	 * 
 	 * @param board scacchiera attuale
 	 * @param start casa di partenza
