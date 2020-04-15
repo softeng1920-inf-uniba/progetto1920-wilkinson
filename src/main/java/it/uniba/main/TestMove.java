d156c5ba0f5c3f27691685d03fbbfff659d1067f
Gianluca Laera <g.laera28@studenti.uniba.it>
2020-04-15 11:48:11 +0200

    Aggiornata classe di test

--- src/main/java/it/uniba/main/TestMove.java
+++ src/main/java/it/uniba/main/TestMove.java
@@ -5,18 +5,18 @@ import java.util.Scanner;
 public class TestMove {
 	
 	public static void main(String[] args) {
-		Board board = new Board();
+		Game game = new Game();
 		String command;
 		System.out.print("Inserire comando: ");
 		try (Scanner scan = new Scanner(System.in)) {
 			command = scan.nextLine();
 		}
 		
-		Move move = new Move(command, board);
+		Move move = new Move(command, game);
 		
 		System.out.println(move.getEnd());
 		System.out.println(move.getStart());
-		//System.out.println(move.getPieceMoved().draw());
+		move.getPieceMoved().draw();
 		
 	}
 }
