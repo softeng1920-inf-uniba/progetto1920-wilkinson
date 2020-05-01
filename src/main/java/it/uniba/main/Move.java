c1fbce8ac67e6f15d4b8379eb8a24aa5e8b6a4c2
Giuseppe Sancesario <giuseppesancesario1999@outlook.com>
2020-05-01 13:54:55 +0200

    implementato arrocco corto
    è stato implementato nel metodo makeCastling della classe Move.java
    controllo sulla possibilità di effettuare un arrocco corto, se è
    possibile l'arrocco viene effettuato.

--- src/main/java/it/uniba/main/Move.java
+++ src/main/java/it/uniba/main/Move.java
@@ -247,7 +247,17 @@ public final class Move {
 		if (game.isWhiteTurn()) {
 			Spot whiteKingSpot = game.getBoard().getSpot(7, 4);
 			if (this.getInterpreter().isCastleShort()) {
-				// TODO arrocco corto (bianchi)
+				Spot whiteDxRookSpot = game.getBoard().getSpot(7, 7);
+				Spot whiteNewKingSpot = game.getBoard().getSpot(7, 6);
+				Spot whiteNewRookSpot = game.getBoard().getSpot(7, 5);
+				if (isCastlePossible(game.getBoard(), whiteNewKingSpot, whiteNewRookSpot, whiteKingSpot,
+						whiteDxRookSpot, null)) {
+					whiteNewKingSpot.setPiece(whiteKingSpot.getPiece());
+					whiteNewRookSpot.setPiece(whiteDxRookSpot.getPiece());
+					whiteKingSpot.setPiece(null);
+					whiteDxRookSpot.setPiece(null);
+					return true;
+				}
 			} else if (this.getInterpreter().isCastleLong()) {
 				// TODO arrocco lungo (bianchi)
 			}
