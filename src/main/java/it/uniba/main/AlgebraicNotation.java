1a545f51cca3a0004654c0b21d2b843f0ffcc8cd
pventrella20 <p.ventrella4@studenti.uniba.it>
2020-05-04 12:56:19 +0200

    Fix parser comando arrocco
    - fixato bug che impediva il riconoscimento dell'arrocco come notazione
    scritta correttamente

--- src/main/java/it/uniba/main/AlgebraicNotation.java
+++ src/main/java/it/uniba/main/AlgebraicNotation.java
@@ -195,7 +195,13 @@ public final class AlgebraicNotation {
 	 * 
 	 * @return true se comando valido, false altrimenti
 	 */
-	public boolean isValidAlgebraicNotation() {
+	public boolean isValidAlgebraicNotation() {	
+		if (isCastleShort()) {
+			return getSymbol().get(0).equals("0-0");
+		} else if (isCastleLong()) {
+			return getSymbol().get(0).equals("0-0-0");
+		}
+		
 		if (this.getPieceLetter() != "") {
 			switch (this.getPieceLetter()) {
 			case "A":
@@ -217,22 +223,8 @@ public final class AlgebraicNotation {
 
 			if (length == MAXLENGTHENDSQ) {
 				switch (square.substring(STARTINDEX, PIECELETTERINDEX)) {
-				case "a":
-				case "b":
-				case "c":
-				case "d":
-				case "e":
-				case "f":
-				case "g":
-				case "h":
-				case "1":
-				case "2":
-				case "3":
-				case "4":
-				case "5":
-				case "6":
-				case "7":
-				case "8":
+				case "a": case "b": case "c": case "d": case "e": case "f": case "g": case "h":
+				case "1": case "2": case "3": case "4": case "5": case "6": case "7": case "8":
 					break;
 				default:
 					return false;
@@ -240,71 +232,15 @@ public final class AlgebraicNotation {
 				square = reduceString(square, square.substring(STARTINDEX, PIECELETTERINDEX));
 			}
 
-			switch (square) {
-			case "a1":
-			case "a2":
-			case "a3":
-			case "a4":
-			case "a5":
-			case "a6":
-			case "a7":
-			case "a8":
-			case "b1":
-			case "b2":
-			case "b3":
-			case "b4":
-			case "b5":
-			case "b6":
-			case "b7":
-			case "b8":
-			case "c1":
-			case "c2":
-			case "c3":
-			case "c4":
-			case "c5":
-			case "c6":
-			case "c7":
-			case "c8":
-			case "d1":
-			case "d2":
-			case "d3":
-			case "d4":
-			case "d5":
-			case "d6":
-			case "d7":
-			case "d8":
-			case "e1":
-			case "e2":
-			case "e3":
-			case "e4":
-			case "e5":
-			case "e6":
-			case "e7":
-			case "e8":
-			case "f1":
-			case "f2":
-			case "f3":
-			case "f4":
-			case "f5":
-			case "f6":
-			case "f7":
-			case "f8":
-			case "g1":
-			case "g2":
-			case "g3":
-			case "g4":
-			case "g5":
-			case "g6":
-			case "g7":
-			case "g8":
-			case "h1":
-			case "h2":
-			case "h3":
-			case "h4":
-			case "h5":
-			case "h6":
-			case "h7":
-			case "h8":
+			switch(square) {
+			case "a1": case "a2": case "a3": case "a4": case "a5": case "a6": case "a7": case "a8":
+			case "b1": case "b2": case "b3": case "b4": case "b5": case "b6": case "b7": case "b8":
+			case "c1": case "c2": case "c3": case "c4": case "c5": case "c6": case "c7": case "c8":
+			case "d1": case "d2": case "d3": case "d4": case "d5": case "d6": case "d7": case "d8":
+			case "e1": case "e2": case "e3": case "e4": case "e5": case "e6": case "e7": case "e8":
+			case "f1": case "f2": case "f3": case "f4": case "f5": case "f6": case "f7": case "f8":
+			case "g1": case "g2": case "g3": case "g4": case "g5": case "g6": case "g7": case "g8":
+			case "h1": case "h2": case "h3": case "h4": case "h5": case "h6": case "h7": case "h8":
 				break;
 			default:
 				return false;
@@ -319,7 +255,8 @@ public final class AlgebraicNotation {
 	public String toString() {
 		String output = "";
 		return output += "Pezzo mosso: " + this.getPieceLetter() + "\nCasa d'arrivo: " + this.getEndSquareId()
-				+ "\nSimboli: " + this.getSymbol();
+				+ "\nSimboli: " + this.getSymbol() + "\nMossa legale: " + this.isGoodMove() + "\nCastleShort: " + this.isCastleShort()
+				+ "\nCastleLong: " + this.isCastleLong() + "\nIsCapture: " + this.isCapture();
 	}
 
 	// Getters & Setters
