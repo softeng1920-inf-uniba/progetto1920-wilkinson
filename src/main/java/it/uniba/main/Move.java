ded672b7c1f10c77f0308d1d7bb71f5ed0479be0
Stefano Lozito <61432874+stefanolozito@users.noreply.github.com>
2020-05-13 17:48:33 +0200

    Update Move.java

--- src/main/java/it/uniba/main/Move.java
+++ src/main/java/it/uniba/main/Move.java
@@ -1,3 +1,4 @@
+
 package it.uniba.main;
 
 import java.util.ArrayList;
@@ -6,7 +7,7 @@ import java.util.ArrayList;
  * DESCRIZIONE
  * rappresenta un movimento di un pezzo degli scacchi
  * da una casa di partenza ad una di arrivo
- * 
+ *
  * RESPONSABILITA' DI CLASSE
  * La classe effettua operazioni a partire da un input interpretato
  * - controllare quale pezzo si vuol muovere
@@ -15,7 +16,7 @@ import java.util.ArrayList;
  * - stabilire se e' possibile una cattura (classica o en-passant)
  * - controllare casi di ambiguita' nel movimento
  * - rigettare l'input nel caso di irregolarita'
- * 
+ *
  * CLASSIFICAZIONE ECB
  * <<Control>>
  * essendo responsabile della logica che interessa il movimento
@@ -29,27 +30,24 @@ public final class Move {
 	private Spot end; // casa di arrivo
 	private Piece pieceMoved; // pezzo che deve eseguire il movimento
 	private boolean isAmbiguity = false; // caso in cui ci sia ambiguita' di movimento
-	private String ambiguity; // primo carattere di un'ambiguitÃ�
-	private static final int X_WHYTE_CASTLING = 7;
-	private static final int Y_WHYTE_BLACK_CASTLING = 4;
-	private static final int X_BLACK_CASTLING = 0;
-	private static final int LINE_SPOTS = 8;
-
+	private String ambiguity; // primo carattere di un'ambiguitÃ 
 
 	/**
 	 * costruttore dell'oggetto Move
-	 * 
+	 *
 	 * @param command comando da interpretare in mossa
 	 * @param game    partita in corso
 	 */
 	public Move(final String command, final Game game) {
 		this.interpreter = new AlgebraicNotation(command); // Istanzio l'oggetto interpreter
-
+		final int kingCol = 4;
+		final int whiteKingRaw = 7;
+		final int blackKingRaw = 0;
 		if (this.isCastle()) { // se e' un arrocco setta le coordinate di partenza del re
 			if (game.isWhiteTurn()) {
-				this.start = new Spot(X_WHYTE_CASTLING, Y_WHYTE_BLACK_CASTLING);
+				this.start = new Spot(whiteKingRaw, kingCol);
 			} else {
-				this.start = new Spot(X_BLACK_CASTLING, Y_WHYTE_BLACK_CASTLING);
+				this.start = new Spot(blackKingRaw, kingCol);
 			}
 		}
 
@@ -68,7 +66,7 @@ public final class Move {
 				// avvalora il pezzo da muovere prendendolo da start
 				this.pieceMoved = start.getPiece();
 				// capisce se e' una cattura en passant
-				if (isEnPassantMove(pieceMoved, start, end, game.getBoard())) {
+				if (isEnPassantMove(game.getBoard())) {
 					((Pawn) start.getPiece()).setCapturingEnPassant(true);
 				}
 			}
@@ -78,7 +76,7 @@ public final class Move {
 	/**
 	 * costruttore secondario di un oggetto Move semplice (solo spot di
 	 * partenza/arrivo)
-	 * 
+	 *
 	 * @param start
 	 * @param end
 	 */
@@ -87,29 +85,30 @@ public final class Move {
 		this.end = inEnd;
 	}
 
-	static final int EXPECTED_COMMAND_LENGTH = 2;
-	static final int EXPECTED_AMBIGUOUS_COMMAND_LENGTH = 3;
-	static final int BOARD_LENGTH = 8;
-	static final int BOARD_HEIGHT = 8;
+	private static final int EXPECTED_COMMAND_LENGTH = 2;
+	private static final int EXPECTED_AMBIGUOUS_COMMAND_LENGTH = 3;
+	private static final int BOARD_LENGTH = 8;
+	private static final int BOARD_HEIGHT = 8;
+	private static final int LETTER_INDEX = 2;
+	private static final int NUMBER_INDEX = 3;
 
 	/**
 	 * estrae le coordinate della casa di arrivo
-	 * 
+	 *
 	 * @param algebraicFinalSpot stringa di coordinate
 	 * @return spot di arrivo instanziato come elemento di classe Spot
 	 */
 	private Spot extractCoordinates(final String algebraicFinalSpot) {
 		Spot endSpot;
-		// caso in cui la stringa sia lunga 2
 		if (algebraicFinalSpot.length() == EXPECTED_COMMAND_LENGTH) {
+			// caso in cui la stringa sia lunga 2
 			endSpot = new Spot(convertCoordinate(algebraicFinalSpot.substring(1, 2)),
 					convertCoordinate(algebraicFinalSpot.substring(0, 1)), null);
 			return endSpot;
-		// caso in cui la stringa sia lunga 3 (ambiguita')
 		} else if (algebraicFinalSpot.length() == EXPECTED_AMBIGUOUS_COMMAND_LENGTH) {
-			endSpot =
-			new Spot(convertCoordinate(algebraicFinalSpot.substring(2, EXPECTED_AMBIGUOUS_COMMAND_LENGTH)),
-					convertCoordinate(algebraicFinalSpot.substring(1, 2)), null);
+			// caso in cui la stringa sia lunga 3 (ambiguita')
+			endSpot = new Spot(convertCoordinate(algebraicFinalSpot.substring(LETTER_INDEX, NUMBER_INDEX)),
+					convertCoordinate(algebraicFinalSpot.substring(1, LETTER_INDEX)), null);
 			ambiguity = algebraicFinalSpot.substring(0, 1);
 
 			setAmbiguity(true);
@@ -121,27 +120,27 @@ public final class Move {
 	/**
 	 * trova lo spot di partenza cercando il pezzo con: 1) casa di arrivo
 	 * corrispodente a quella inserita 2) controlla solo i pezzi della classe
-	 * inserita dall'utente 3) controlla eventuali ambiguita' 
-	 * 
+	 * inserita dall'utente 3) controlla eventuali ambiguita' 
+	 *
 	 * @param board
 	 * @param piece
 	 * @return
 	 */
 	boolean findStartSpot(final Board board, final Piece piece, final boolean turn) {
 		ArrayList<Piece> piecesMovable = new ArrayList<Piece>();
-		for (int i = 0; i < LINE_SPOTS; i++) {
-			for (int j = 0; j < LINE_SPOTS; j++) {
+		for (int i = 0; i < BOARD_LENGTH; i++) {
+			for (int j = 0; j < BOARD_HEIGHT; j++) {
 				Spot currentSpot = board.getSpot(i, j);
-				if (currentSpot.getPiece() != null
-						&& currentSpot.getPiece().getClass().getName()
-						   == piece.getClass().getName()
+				if (!currentSpot.isEmpty()
+						&& currentSpot.getPiece().getClass().getName().equals(
+							piece.getClass().getName())
 						&& currentSpot.getPiece().isWhite() == turn) {
 					for (Move currentMove : currentSpot.getPiece().getLegalMoves()) {
 						if (isAmbiguity) {
 							if (sameEnd(currentMove)
 									&& samePartialStart(currentMove, ambiguity)) {
-								  piecesMovable.add(currentSpot.getPiece());
-								  this.setStart(currentMove.getStart());
+								piecesMovable.add(currentSpot.getPiece());
+								this.setStart(currentMove.getStart());
 							}
 						} else {
 							if (sameEnd(currentMove)) {
@@ -159,53 +158,56 @@ public final class Move {
 		return false;
 	}
 
+	static final int COLA_RAW1 = 0;
+	static final int COLB_RAW2 = 1;
+	static final int COLC_RAW3 = 2;
+	static final int COLD_RAW4 = 3;
+	static final int COLE_RAW5 = 4;
+	static final int COLF_RAW6 = 5;
+	static final int COLG_RAW7 = 6;
+	static final int COLH_RAW8 = 7;
+	static final int NOT_ON_BOARD = 8;
+
 	/**
 	 * converte la coordinata in notazione matriciale
-	 * 
+	 *
 	 * @param coordinate stringa di coordinate
 	 * @return indice di riga-colonna
 	 */
-	private static final int THREE = 3;
-	private static final int FOUR = 4;
-	private static final int FIVE = 5;
-	private static final int SIX = 6;
-	private static final int SEVEN = 7;
-	private static final int EIGHT = 8;
 	private int convertCoordinate(final String coordinate) {
 		switch (coordinate) {
 		case "a":
 		case "8":
-			return 0;
+			return COLA_RAW1;
 		case "b":
 		case "7":
-			return 1;
+			return COLB_RAW2;
 		case "c":
 		case "6":
-			return 2;
+			return COLC_RAW3;
 		case "d":
 		case "5":
-			return THREE;
+			return COLD_RAW4;
 		case "e":
 		case "4":
-			return FOUR;
+			return COLE_RAW5;
 		case "f":
 		case "3":
-			return FIVE;
+			return COLF_RAW6;
 		case "g":
 		case "2":
-			return SIX;
+			return COLG_RAW7;
 		case "h":
 		case "1":
-			return SEVEN;
+			return COLH_RAW8;
 		default:
-			return EIGHT;
+			return NOT_ON_BOARD;
 		}
-
 	}
 
 	/**
 	 * Intepreta la lettera inserita dall'utente e la converte nel pezzo mosso
-	 * 
+	 *
 	 * @param algebraicPiece pezzo mosso in notazione algebrica
 	 * @return ritorna il pezzo che si deve muovere
 	 */
@@ -237,7 +239,7 @@ public final class Move {
 	/**
 	 * enumerazione dello stato di gioco (per verificare se la partita e' ancora in
 	 * corso)
-	 * 
+	 *
 	 * @author wilkinson
 	 *
 	 */
@@ -247,18 +249,20 @@ public final class Move {
 
 	/**
 	 * capisce se il pezzo che sta muovendo sta catturando en passant
-	 * 
+	 *
 	 * @param piece pezzo mosso
 	 * @param start casa di partenza
 	 * @param end   casa di arrivo
 	 * @param board scacchiera
 	 * @return true se cattura en passant, false altrimenti
 	 */
-	boolean isEnPassantMove(final Piece enPiece, final Spot enStart, final Spot enEnd, final Board enBoard) {
-	  if (enPiece instanceof Pawn && enBoard.isFrontDiagonal(enStart, enEnd) && enEnd.getPiece() == null
-			&& enBoard.getSpot(enStart.getX(), enEnd.getY()).getPiece() instanceof Pawn
-			&& ((Pawn) enBoard.getSpot(enStart.getX(),
-				enEnd.getY()).getPiece()).isPossibleEnPassantCapture()) {
+	boolean isEnPassantMove(final Board board) {
+		if (pieceMoved instanceof Pawn
+				&& board.isFrontDiagonal(start, end)
+				&& board.getSpot(end.getX(), end.getY()).isEmpty()
+				&& board.getSpot(start.getX(), end.getY()).getPiece() instanceof Pawn
+				&& ((Pawn) board.getSpot(start.getX(), end.getY())
+						.getPiece()).isPossibleEnPassantCapture()) {
 			return true;
 		}
 		return false;
@@ -266,7 +270,7 @@ public final class Move {
 
 	/**
 	 * controlla se l'input e' un arrocco
-	 * 
+	 *
 	 * @return
 	 */
 	boolean isCastle() {
@@ -276,19 +280,30 @@ public final class Move {
 		return false;
 	}
 
+	private static final int RAW_1 = 7;
+	private static final int RAW_8 = 0;
+	private static final int COL_H = 7;
+	private static final int COL_G = 6;
+	private static final int COL_F = 5;
+	private static final int COL_E = 4;
+	private static final int COL_D = 3;
+	private static final int COL_C = 2;
+	private static final int COL_B = 1;
+	private static final int COL_A = 0;
+
 	/**
 	 * controlla se e' possibile arroccare ed effettua l'arrocco
-	 * 
+	 *
 	 * @param game
 	 * @return
 	 */
 	boolean makeCastling(final Game game) {
 		if (game.isWhiteTurn()) {
-			Spot whiteKingSpot = game.getBoard().getSpot(SEVEN, FOUR);
+			Spot whiteKingSpot = game.getBoard().getSpot(RAW_8, COL_E);
 			if (this.getInterpreter().isCastleShort()) { // arrocco corto bianco
-				Spot whiteDxRookSpot = game.getBoard().getSpot(SEVEN, SEVEN);
-				Spot whiteNewKingSpot = game.getBoard().getSpot(SEVEN, SIX);
-				Spot whiteNewRookSpot = game.getBoard().getSpot(SEVEN, FIVE);
+				Spot whiteDxRookSpot = game.getBoard().getSpot(RAW_8, COL_H);
+				Spot whiteNewKingSpot = game.getBoard().getSpot(RAW_8, COL_G);
+				Spot whiteNewRookSpot = game.getBoard().getSpot(RAW_8, COL_F);
 				if (isCastlePossible(game.getBoard(), whiteNewKingSpot, whiteNewRookSpot, whiteKingSpot,
 						whiteDxRookSpot, null)) {
 					whiteNewKingSpot.setPiece(whiteKingSpot.getPiece());
@@ -298,10 +313,10 @@ public final class Move {
 					return true;
 				}
 			} else if (this.getInterpreter().isCastleLong()) { // arrocco lungo bianco
-				Spot whiteSxRookSpot = game.getBoard().getSpot(SEVEN, 0);
-				Spot whiteNewKingSpot = game.getBoard().getSpot(SEVEN, 2);
-				Spot whiteNewRookSpot = game.getBoard().getSpot(SEVEN, THREE);
-				Spot knightSpot = game.getBoard().getSpot(SEVEN, 1);
+				Spot whiteSxRookSpot = game.getBoard().getSpot(RAW_8, COL_A);
+				Spot whiteNewKingSpot = game.getBoard().getSpot(RAW_8, COL_C);
+				Spot whiteNewRookSpot = game.getBoard().getSpot(RAW_8, COL_D);
+				Spot knightSpot = game.getBoard().getSpot(RAW_8, COL_B);
 				if (isCastlePossible(game.getBoard(), whiteNewKingSpot, whiteNewRookSpot, whiteKingSpot,
 						whiteSxRookSpot, knightSpot)) {
 					whiteNewKingSpot.setPiece(whiteKingSpot.getPiece());
@@ -312,11 +327,11 @@ public final class Move {
 				}
 			}
 		} else {
-			Spot blackKingSpot = game.getBoard().getSpot(0, FOUR);
+			Spot blackKingSpot = game.getBoard().getSpot(RAW_1, COL_E);
 			if (this.getInterpreter().isCastleShort()) { // arrocco corto nero
-				Spot blackDxRookSpot = game.getBoard().getSpot(0, SEVEN);
-				Spot blackNewKingSpot = game.getBoard().getSpot(0, SIX);
-				Spot blackNewRookSpot = game.getBoard().getSpot(0, FIVE);
+				Spot blackDxRookSpot = game.getBoard().getSpot(RAW_1, COL_H);
+				Spot blackNewKingSpot = game.getBoard().getSpot(RAW_1, COL_G);
+				Spot blackNewRookSpot = game.getBoard().getSpot(RAW_1, COL_F);
 				if (isCastlePossible(game.getBoard(), blackNewKingSpot, blackNewRookSpot, blackKingSpot,
 						blackDxRookSpot, null)) {
 					blackNewKingSpot.setPiece(blackKingSpot.getPiece());
@@ -326,10 +341,10 @@ public final class Move {
 					return true;
 				}
 			} else if (this.getInterpreter().isCastleLong()) { // arrocco lungo nero
-				Spot blackSxRookSpot = game.getBoard().getSpot(0, 0);
-				Spot blackNewKingSpot = game.getBoard().getSpot(0, 2);
-				Spot blackNewRookSpot = game.getBoard().getSpot(0, THREE);
-				Spot knightSpot = game.getBoard().getSpot(0, 1);
+				Spot blackSxRookSpot = game.getBoard().getSpot(RAW_1, COL_A);
+				Spot blackNewKingSpot = game.getBoard().getSpot(RAW_1, COL_C);
+				Spot blackNewRookSpot = game.getBoard().getSpot(RAW_1, COL_D);
+				Spot knightSpot = game.getBoard().getSpot(RAW_1, COL_B);
 				if (isCastlePossible(game.getBoard(), blackNewKingSpot, blackNewRookSpot, blackKingSpot,
 						blackSxRookSpot, knightSpot)) {
 					blackNewKingSpot.setPiece(blackKingSpot.getPiece());
@@ -345,7 +360,7 @@ public final class Move {
 
 	/**
 	 * controlla se le case fra re e torre sono sotto attacco
-	 * 
+	 *
 	 * @param board
 	 * @param kingSpot
 	 * @param rookSpot
@@ -364,7 +379,7 @@ public final class Move {
 
 	/**
 	 * controlla se le case fra re e torre sono libere da pezzi
-	 * 
+	 *
 	 * @param board
 	 * @param kingSpot
 	 * @param rookSpot
@@ -386,7 +401,7 @@ public final class Move {
 
 	/**
 	 * controlla se re e torre non sono stati mossi
-	 * 
+	 *
 	 * @param king
 	 * @param rook
 	 * @return
@@ -400,7 +415,7 @@ public final class Move {
 
 	/**
 	 * controlla che i pezzi nelle case di partenza di re e torre siano giusti
-	 * 
+	 *
 	 * @param king
 	 * @param rook
 	 * @return
@@ -413,9 +428,10 @@ public final class Move {
 		}
 		return false;
 	}
+
 	/**
 	 * applica una serie di controlli per stabilire se l'arrocco è possibile
-	 * 
+	 *
 	 * @param board
 	 * @param king
 	 * @param rook
@@ -440,27 +456,45 @@ public final class Move {
 
 	/**
 	 * confronta due oggetti di tipo Move e controlla se hanno le stesse coordinate
-	 * 
+	 *
 	 * @param move
 	 * @return true se hanno le stesse coordinate, false altrimenti
 	 */
-	public boolean equals(final Move move) {
-		if (this.getEnd().getX() == move.getEnd().getX() && this.getEnd().getY() == move.getEnd().getY()
-				&& this.getStart().getX() == move.getStart().getX()
-				&& this.getStart().getY() == move.getStart().getY()) {
+	@Override
+	public boolean equals(final Object move) {
+		if (!(move instanceof Move)) {
+			return false;
+		}
+		Move inMove = (Move) move;
+		if (this.getEnd().getX() == inMove.getEnd().getX()
+				&& this.getEnd().getY() == inMove.getEnd().getY()
+				&& this.getStart().getX() == inMove.getStart().getX()
+				&& this.getStart().getY() == inMove.getStart().getY()) {
 			return true;
 		}
 		return false;
 	}
 
+	private static final int HASH = 42;
+
+	/**
+	 * default hashcode (di nessuna utilita', poiche' non c'e' un Set implementato)
+	 */
+	@Override
+	public int hashCode() {
+	    assert false : "hashCode not designed";
+	    return HASH; // any arbitrary constant will do
+	}
+
 	/**
 	 * controlla se due oggetti di tipo move hanno la stessa casa di arrivo
-	 * 
+	 *
 	 * @param move
 	 * @return
 	 */
 	public boolean sameEnd(final Move move) {
-		if (this.getEnd().getX() == move.getEnd().getX() && this.getEnd().getY() == move.getEnd().getY()) {
+		if (this.getEnd().getX() == move.getEnd().getX()
+				&& this.getEnd().getY() == move.getEnd().getY()) {
 			return true;
 		}
 		return false;
@@ -469,7 +503,7 @@ public final class Move {
 	/**
 	 * controlla se la casa di start della mossa corrisponde parzialmente
 	 * all'ambiguita'
-	 * 
+	 *
 	 * @param move
 	 * @param coord
 	 * @return
@@ -508,8 +542,8 @@ public final class Move {
 		return isAmbiguity;
 	}
 
-	public void setAmbiguity(final boolean inAmbiguity) {
-		this.isAmbiguity = inAmbiguity;
+	public void setAmbiguity(final boolean inIsAmbiguity) {
+		this.isAmbiguity = inIsAmbiguity;
 	}
 
 	public void setStart(final Spot inStart) {
