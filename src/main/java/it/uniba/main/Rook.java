b7c6f35c8aa6030bf2f90e645892b8720f3370a4
stefanolozito <steve.lozito98@gmail.com>
2020-05-13 17:40:06 +0200

    Corretti errori SpotBugs/CheckStyle per la classe Rook

--- src/main/java/it/uniba/main/Rook.java
+++ src/main/java/it/uniba/main/Rook.java
@@ -2,23 +2,27 @@ package it.uniba.main;
 
 /**
  * DESCRIZIONE
- * rappresenta un pezzo torre 
+ * rappresenta un pezzo torre
  *
  * RESPONSABILITA' DI CLASSE
- * calcola le mosse legali di una torre seguendo le regole ufficiale degli scacchi
+ * calcola le mosse legali di una torre seguendo le
+ * regole ufficiale degli scacchi
  *
  * CLASSIFICAZIONE ECB
- * <<Entity>>
- * poiche' eredita dalla classe Piece.java
- * 
+ * <<Entity>> poiche' eredita dalla classe Piece.java
+ *
  * @author wilkinson
  */
 public class Rook extends Piece {
 
-	public Rook(boolean white) {
+	public Rook(final boolean white) {
 		super(white);
 	}
 
+	/**
+	 * Metodo per ottenere l'unicode della torre in base al suo colore (bianco o
+	 * nero)
+	 */
 	@Override
 	public String draw() {
 		if (isWhite()) {
@@ -28,8 +32,16 @@ public class Rook extends Piece {
 		}
 	}
 
+	/**
+	 * metodo che stabilisce se il pezzo puo' muoversi (la mossa e' legale)
+	 *
+	 * @param board scacchiera attuale
+	 * @param start casa di partenza
+	 * @param end   casa di arrivo
+	 * @return true se movimento possibile, false se mossa illegale
+	 */
 	@Override
-	boolean canMove(Board board, Spot start, Spot end) {
+	boolean canMove(final Board board, final Spot start, final Spot end) {
 		if (board.isFreePath(start, end)) {
 			return true;
 		}
