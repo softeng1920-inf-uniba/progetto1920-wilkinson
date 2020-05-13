273deed3bffdc5969fce04f2b0b08e33cc2c7bfc
Nicola Ragone <nicola.ra96@gmail.com>
2020-05-13 19:19:59 +0200

    Create directory classi di test

---
+++ src/test/java/it/uniba/example/utils/test/MathUtilsTest.java
@@ -0,0 +1,26 @@
+package it.uniba.example.utils.test;
+
+import static org.junit.Assume.assumeNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotSame;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+/**
+ * This class is for demo purpose only and must be deleted. It is meant to show
+ * you how to create your own JUnit 5 test cases for your code.
+ */
+@Tag("util")
+public class MathUtilsTest {
+
+}
---
+++ src/test/java/it/uniba/example/utils/test/StringUtilsTest.java
@@ -0,0 +1,22 @@
+package it.uniba.example.utils.test;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+/**
+ * This class is for demo purpose only and must be deleted. It is meant to show
+ * you how to create your own JUnit 5 test cases for your code.
+ */
+public class StringUtilsTest {
+
+}
