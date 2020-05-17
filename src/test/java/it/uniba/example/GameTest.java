8faaada6d2e9cf0e736c7d2a9bcf07ecfe03864b
Nicola Ragone <nicola.ra96@gmail.com>
2020-05-17 21:21:07 +0200

    Implementato caso di test booleano della torre e relativa cattura

--- src/test/java/it/uniba/example/GameTest.java
+++ src/test/java/it/uniba/example/GameTest.java
@@ -11,6 +11,8 @@ import it.uniba.main.Pawn;
 import it.uniba.main.Queen;
 
 class GameTest {
+	
+	//RIGHE
 //	private static final int ROW_1 = 7;
 //	private static final int ROW_2 = 6;
 	private static final int ROW_3 = 5;
@@ -20,9 +22,10 @@ class GameTest {
 //	private static final int ROW_7 = 1;
 //	private static final int ROW_8 = 0;
 	
-//	private static final int COL_H = 7;
+	//COLONNE
+	private static final int COL_H = 7;
 	private static final int COL_G = 6;
-//	private static final int COL_F = 5;
+	private static final int COL_F = 5;
 	private static final int COL_E = 4;
 	private static final int COL_D = 3;
 //	private static final int COL_C = 2;
@@ -32,16 +35,16 @@ class GameTest {
 	@Test
 	void testCurrentGame() {
 		Game game = new Game();
-		
+
 		//test isMoved pedone
 		game.currentGame("d4");
 		assertTrue(game.getBoard().getSpot(ROW_4, COL_D).getPiece().isMoved());
-		
+
 		//test cattura pedone
 		game.currentGame("e5");
 		game.currentGame("dxe5");
 		assertTrue(game.getBoard().getSpot(ROW_5, COL_E).getPiece() instanceof Pawn);
-		
+
 		//test isMoved Regina
 		game.currentGame("d5");
 		game.currentGame("Dxd5");
@@ -49,23 +52,39 @@ class GameTest {
 
 		//test cattura Regina
 		assertTrue(game.getBoard().getSpot(ROW_5, COL_D).getPiece() instanceof Queen);
-		
+
 		//test  isMoved Cavallo
 		game.currentGame("h5");
 		game.currentGame("Ca3");
 		assertTrue(game.getBoard().getSpot(ROW_3, COL_A).getPiece().isMoved());
-		
+
 		//test cattura Cavallo
 		game.currentGame("b5");
 		game.currentGame("Cxb5");
 		assertTrue(game.getBoard().getSpot(ROW_5, COL_B).getPiece() instanceof Knight);
-		
+
 		//test isMoved Alfiere
 		game.currentGame("g5");
 		game.currentGame("Axg5");
 		assertTrue(game.getBoard().getSpot(ROW_5, COL_G).getPiece().isMoved());
-		
+
 		//test cattura Alfiere
 		assertTrue(game.getBoard().getSpot(ROW_5, COL_G).getPiece() instanceof Bishop);
+
+		//test isMoved Torre
+		game.currentGame("Ch6");
+		game.currentGame("h4");
+		game.currentGame("Ag7");
+		game.currentGame("Th3");
+		assertTrue(game.getBoard().getSpot(ROW_3, COL_H).getPiece().isMoved());
+		
+		//test cattura Torre
+		game.currentGame("f5");
+		game.currentGame("a4");
+		game.currentGame("f4");
+		game.currentGame("a5");
+		game.currentGame("f3");
+		game.currentGame("Txf3");
+		assertTrue(game.getBoard().getSpot(ROW_3, COL_F).getPiece() instanceof Bishop);
 	}
 }
