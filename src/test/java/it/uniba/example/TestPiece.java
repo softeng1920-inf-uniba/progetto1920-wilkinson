f6be02cba2cbf4e56d2315836e039e2ff9f71bb4
Vincenzo Martemucci <vincenzomartemucci@protonmail.com>
2020-05-18 19:03:03 +0200

    aggiunti test di movimento per il pedone

--- src/test/java/it/uniba/example/TestPiece.java
+++ src/test/java/it/uniba/example/TestPiece.java
@@ -44,80 +44,40 @@ public class TestPiece {
 	private static final int COL_B = 1;
 	private static final int COL_A = 0;
 
-	
-	
-	@BeforeEach
-	void setup() {
+	@Test
+	void testPawnMovement() {
 		board = new Board(true);
-		//board.getSpot(ROW_1, COL_E).setPiece(new King(WHITE));
+		// caso di test per il movimento del pedone
+		board.getSpot(ROW_2, COL_A).setPiece(new Pawn(WHITE));
+		// movimento in avanti di una casella
+		assertTrue(board.getSpot(ROW_2, COL_A).getPiece().canMove(board, board.getSpot(ROW_2, COL_A), board.getSpot(ROW_3, COL_A)));
+		// movimento in avanti di due caselle
+		assertTrue(board.getSpot(ROW_2, COL_A).getPiece().canMove(board, board.getSpot(ROW_2, COL_A), board.getSpot(ROW_4, COL_A)));
+		// movimento in avanti di tre caselle
+		assertFalse(board.getSpot(ROW_2, COL_A).getPiece().canMove(board, board.getSpot(ROW_2, COL_A), board.getSpot(ROW_5, COL_A)));
+		// movimento indietro
+		assertFalse(board.getSpot(ROW_2, COL_A).getPiece().canMove(board, board.getSpot(ROW_2, COL_A), board.getSpot(ROW_1, COL_A)));
+
+		// movimento in diagonale (senza pezzi nemici)
+		assertFalse(board.getSpot(ROW_2, COL_A).getPiece().canMove(board, board.getSpot(ROW_2, COL_A), board.getSpot(ROW_3, COL_B)));
+		// movimento in diagonale (con pezzo nemico)
+		board.getSpot(ROW_3, COL_B).setPiece(new Pawn(BLACK)); // pezzo nemico
+		assertTrue(board.getSpot(ROW_2, COL_A).getPiece().canMove(board, board.getSpot(ROW_2, COL_A), board.getSpot(ROW_3, COL_B)));
+		// movimento in diagonale (con pezzo amico)
+		board.getSpot(ROW_3, COL_B).setPiece(new Pawn(WHITE)); // pezzo amico
+		assertFalse(board.getSpot(ROW_2, COL_A).getPiece().canMove(board, board.getSpot(ROW_2, COL_A), board.getSpot(ROW_3, COL_B)));
+
+		// movimento in avanti con pedone di fronte
+		board.getSpot(ROW_3, COL_A).setPiece(new Pawn(WHITE));
+		assertFalse(board.getSpot(ROW_2, COL_A).getPiece().canMove(board, board.getSpot(ROW_2, COL_A), board.getSpot(ROW_3, COL_A)));
+		// movimento in avanti di due caselle di un pedone gia' mosso
+		board.getSpot(ROW_3, COL_A).getPiece().setAsMoved();
+		assertFalse(board.getSpot(ROW_3, COL_A).getPiece().canMove(board, board.getSpot(ROW_3, COL_A), board.getSpot(ROW_5, COL_A)));
 	}
 
-	@Test
-	public void testPawnLegalMovements() {
-		/*
-		 * io farei in questo modo: - scacchiera vuota - genero un pezzo - calcolo le
-		 * sue mosse legali - confronto la size dell'arraylist (deve essere pari al
-		 * numero di mosse legali) - confronto le mosse dell'arraylist con quelle legali
-		 * che so a priori
-		 */
-
-		Spot blackPawnSpot = board.getSpot(ROW_7, COL_B);
-		blackPawnSpot.setPiece(new Pawn(BLACK));
-		Piece blackPawn = blackPawnSpot.getPiece();
-		blackPawnSpot.setPiece(new Pawn(BLACK));
-		blackPawn.findLegalMoves(board, blackPawnSpot);
-		
-		
-		//test moved
-		assertFalse(blackPawn.isMoved());
-		// test sulla lunghezza dell'arraylist di mosse (in questo caso 2 -> b6, b5)
-		assertEquals(blackPawn.getLegalMoves().size(), 2);
-		// test sulle mosse presenti all'interno dell'arrayList di mosse
-		assertTrue(blackPawn.getLegalMoves().contains(new Move(blackPawnSpot, new Spot(ROW_6, COL_B))));
-		assertTrue(blackPawn.getLegalMoves().contains(new Move(blackPawnSpot, new Spot(ROW_5, COL_B))));
-		
-		Spot whitePawnSpot = board.getSpot(ROW_2, COL_C);
-		whitePawnSpot.setPiece(new Pawn(WHITE));
-		Piece whitePawn = whitePawnSpot.getPiece();
-		whitePawnSpot.setPiece(new Pawn(WHITE));
-		whitePawn.findLegalMoves(board, whitePawnSpot);
-		
-		
-		board.showBoard();
-
-		// test sulla lunghezza dell'arraylist di mosse (in questo caso 2 -> c3, c4)
-		assertEquals(whitePawn.getLegalMoves().size(), 2);
-		// test sulle mosse presenti all'interno dell'arrayList di mosse
-		assertTrue(whitePawn.getLegalMoves().contains(new Move(whitePawnSpot, new Spot(ROW_3, COL_C))));
-		assertTrue(whitePawn.getLegalMoves().contains(new Move(whitePawnSpot, new Spot(ROW_4, COL_C))));
-		//board.showBoard();
-	}
-	
 	
 
-	@Test
-	public void testKingLegalMovements() {
-		// MOVIMENTI DEL RE
-		Spot currentSpot = board.getSpot(ROW_7, COL_B);
-		currentSpot.setPiece(new King(WHITE));
-		Piece currentPiece = currentSpot.getPiece();
-		currentSpot.setPiece(new King(WHITE));
-		currentPiece.findLegalMoves(board, currentSpot);
-		// test sulla lunghezza dell'arraylist di mosse (in questo caso 8 -> b6, b5)
-		//board.showBoard();
-		assertEquals(currentPiece.getLegalMoves().size(), 8);
-		// test sulle mosse presenti all'interno dell'arrayList di mosse
-		assertTrue(currentPiece.getLegalMoves().contains(new Move(currentSpot, new Spot(ROW_6, COL_B))));
-		assertTrue(currentPiece.getLegalMoves().contains(new Move(currentSpot, new Spot(ROW_8, COL_B))));
-		assertTrue(currentPiece.getLegalMoves().contains(new Move(currentSpot, new Spot(ROW_6, COL_A))));
-		assertTrue(currentPiece.getLegalMoves().contains(new Move(currentSpot, new Spot(ROW_7, COL_A))));
-		assertTrue(currentPiece.getLegalMoves().contains(new Move(currentSpot, new Spot(ROW_8, COL_A))));
-		assertTrue(currentPiece.getLegalMoves().contains(new Move(currentSpot, new Spot(ROW_6, COL_C))));
-		assertTrue(currentPiece.getLegalMoves().contains(new Move(currentSpot, new Spot(ROW_7, COL_C))));
-		assertTrue(currentPiece.getLegalMoves().contains(new Move(currentSpot, new Spot(ROW_8, COL_C))));
-
-	}
-
+	
 	@Test
 	public void testKingRecalculateMovements() {
 		Spot kingSpot = board.getSpot(ROW_7, COL_B);
@@ -145,7 +105,7 @@ public class TestPiece {
 		rookSpot.setPiece(new Rook(BLACK));
 		rookPiece.findLegalMoves(board, rookSpot);
 
-		// test sulla lunghezza dell'arraylist di mosse (in questo caso 8 -> b6, b5)
+		// test sulla lunghezza dell'arraylist di mosse (in questo caso 5 -> b6, b5)
 		//board.showBoard();
 		kingPiece.recalculateMoves(board);
 		assertEquals(kingPiece.getLegalMoves().size(), 5);
