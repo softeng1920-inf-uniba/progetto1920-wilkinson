5d918bdab17c9a266671cb63d6fd416b1fb92179
Gianluca Laera <g.laera28@studenti.uniba.it>
2020-05-18 16:26:26 +0200

    test arrocco con pezzo nemico tra torre e re in MoveTest.java

--- src/test/java/it/uniba/example/MoveTest.java
+++ src/test/java/it/uniba/example/MoveTest.java
@@ -137,6 +137,14 @@ class MoveTest {
 		assertFalse(move.makeCastling(board, WHITE));
 	}
 
+	@Test
+	void testCastleEnemyPieceBetween() {
+		move = new Move("0-0", board, WHITE);
+		board.getSpot(ROW_1, COL_H).setPiece(new Rook (WHITE));
+		board.getSpot(ROW_1, COL_F).setPiece(new Queen(BLACK));
+		assertFalse(move.makeCastling(board, WHITE));
+	}
+
 	@After
 	void tearDown() {
 
