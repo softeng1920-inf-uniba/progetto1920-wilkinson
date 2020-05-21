bae016f7dfe8afbb751e0625e233b0220fc42015
Vincenzo Martemucci <vincenzomartemucci@protonmail.com>
2020-05-21 16:48:32 +0200

    Aggiunti casi di test su pedone bianco di movimenti en Passant

--- src/test/java/it/uniba/example/BoardTest.java
+++ src/test/java/it/uniba/example/BoardTest.java
@@ -136,4 +136,26 @@ public class BoardTest {
 				);
 		isRefine = false;
 	}
+	
+	@Test
+	void testWhitePawnEnPassantNotPossibleLegalMoves() {
+		board.getSpot(ROW_5, COL_D).setPiece(new Pawn(WHITE)); // pedone esaminato (d5)
+		examinedPiece = board.getSpot(ROW_5, COL_D).getPiece();
+		board.getSpot(ROW_5, COL_E).setPiece(new Pawn(BLACK)); // pedone nemico    (e5)
+		board.getSpot(ROW_5, COL_D).getPiece().setAsMoved(); // pedone in (d5) gia' mosso
+		board.getSpot(ROW_5, COL_E).getPiece().setAsMoved(); // pedone in (e5) gia' mosso
+		// pedone in (e5) NON PIU' catturabile en passant (es. mosso turno precedente)
+		((Pawn) (board.getSpot(ROW_5, COL_E).getPiece())).setPossibleEnPassantCapture(false);
+		board.recalLegalMoves();
+		assertAll(
+				// mosse del pedone bianco possibili: [1]
+				() -> assertEquals(examinedPiece.getLegalMoves().size(), 1),
+				() -> assertTrue(examinedPiece.getLegalMoves().contains(
+						new Move(new Spot(ROW_5, COL_D), new Spot(ROW_6, COL_D)))), // 1.(d5->d6)
+				// cattura en passant, movimento non possibile
+				() -> assertFalse(examinedPiece.getLegalMoves().contains(
+						new Move(new Spot(ROW_5, COL_D), new Spot(ROW_6, COL_E))))  // (d5->e6) NO!
+				);
+		isRefine = false;
+	}
 }
