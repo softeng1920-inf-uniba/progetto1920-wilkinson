41fbe402b45baeb3a50acf1afee2cf81492cd278
Vincenzo Martemucci <vincenzomartemucci@protonmail.com>
2020-05-21 17:16:02 +0200

    rimossi import non piu usati

--- src/test/java/it/uniba/example/PieceTest.java
+++ src/test/java/it/uniba/example/PieceTest.java
@@ -1,6 +1,5 @@
 package it.uniba.example;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.BeforeEach;
@@ -11,11 +10,8 @@ import it.uniba.main.Board;
 import it.uniba.main.King;
 import it.uniba.main.Queen;
 import it.uniba.main.Knight;
-import it.uniba.main.Move;
 import it.uniba.main.Pawn;
-import it.uniba.main.Piece;
 import it.uniba.main.Rook;
-import it.uniba.main.Spot;
 
 public class PieceTest {
 	private static Board board;
