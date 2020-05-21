ec8326f783e1db2fd0781842aafd2b9673c17704
Vincenzo Martemucci <vincenzomartemucci@protonmail.com>
2020-05-21 16:37:40 +0200

    creata nuova classe di test per Board

---
+++ src/test/java/it/uniba/example/BoardTest.java
@@ -0,0 +1,29 @@
+package it.uniba.example;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import it.uniba.main.Bishop;
+import it.uniba.main.Board;
+import it.uniba.main.King;
+import it.uniba.main.Knight;
+import it.uniba.main.Move;
+import it.uniba.main.Pawn;
+import it.uniba.main.Piece;
+import it.uniba.main.Queen;
+import it.uniba.main.Rook;
+import it.uniba.main.Spot;
+
+public class BoardTest {
+
+	public BoardTest() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
